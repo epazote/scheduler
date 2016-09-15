4cf2dc56cbb9c331b670b254e4edbf3e4c7d780d
nbari <nbari@tequila.io>
2016-09-15 23:05:10 +0200

    modified:   scheduler_test.go

--- scheduler_test.go
+++ scheduler_test.go
@@ -11,19 +11,22 @@ type count struct {
 	i int64
 }
 
-func (c *count) Add() {
-	atomic.AddInt64(&c.i, 1)
+func (c *count) Add(v int64) {
+	atomic.AddInt64(&c.i, v)
+}
+func (c *count) Get() int64 {
+	return atomic.LoadInt64(&c.i)
 }
 
 func TestScheduler(t *testing.T) {
 	sk := New()
 	c := &count{}
-	sk.AddScheduler("print", 1, func() { fmt.Println(atomic.LoadInt64(&c.i)); c.Add() })
+	sk.AddScheduler("print", 1, func() { fmt.Println(c.Get()); c.Add(1) })
 	select {
 	case <-time.After(3 * time.Second):
 		sk.StopAll()
 	}
-	if atomic.LoadInt64(&c.i) != 3 {
+	if c.Get() != 3 {
 		t.Error("Expecting 3")
 	}
 }
